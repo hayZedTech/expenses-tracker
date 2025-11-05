627f557acbba52c24364d83681f1af479cd7e329
Azeez Ololade <ololadeazeez.m@gmail.com>
2025-11-05 10:30:10 +0100

    Initial commit

--- src/contexts/useAuthStore.ts
+++ src/contexts/useAuthStore.ts
@@ -1,9 +1,14 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
+type User = {
+  email: string;
+  fullname?: string;
+};
+
 type AuthState = {
-  user: { email: string; fullname?: string } | null;
-  setUser: (user: { email: string; fullname?: string } | null) => void;
+  user: User | null;
+  setUser: (user: User | null) => void;
   clearUser: () => void;
 };
 
@@ -15,7 +20,8 @@ export const useAuthStore = create<AuthState>()(
       clearUser: () => set({ user: null }),
     }),
     {
-      name: 'auth-storage', // Key used in localStorage
+      name: 'auth-storage', // localStorage key
+      
     }
   )
 );
