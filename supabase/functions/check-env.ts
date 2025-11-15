2f5d9a0561e6fc6188ac0c9b4a2fbcc72ed4cdac
Azeez Ololade <ololadeazeez.m@gmail.com>
2025-11-15 07:02:19 +0100

    Remove supabase/functions folder from Git

--- supabase/functions/check-env.ts
+++
@@ -1,8 +0,0 @@
-import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
-
-const env = config({ path: "./.env" });
-
-console.log({
-  SUPABASE_URL_exists: Boolean(env.SUPABASE_URL),
-  SUPABASE_SERVICE_ROLE_KEY_exists: Boolean(env.SUPABASE_SERVICE_ROLE_KEY),
-});
